49d3f2fef0db86e13583dcdbf80f8b6e02bdb00d
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2020-12-11 00:28:36 +0100

    Using footer in the main class - with proper Enter key handling

--- pymmanderv2/pymander.py
+++ pymmanderv2/pymander.py
@@ -1,5 +1,6 @@
 import urwid
 from pane import Pane
+from footer import Footer
 
 LEFT_ARROW = "<<"
 RIGHT_ARROW = ">>"
@@ -9,7 +10,7 @@ class PyMander(urwid.Frame):
         self.panes = urwid.AttrMap(urwid.Columns([Pane(), Pane()], 2), 'columns')
         self.active_pane = 0
         self.arrow = LEFT_ARROW
-        super(PyMander, self).__init__(self.panes)
+        super(PyMander, self).__init__(self.panes, footer=urwid.BoxAdapter(Footer(), 2))
         self._w = urwid.AttrMap(self, 'pymander')
 
     def set_active_pane(self, pane_number: int):
@@ -20,12 +21,42 @@ class PyMander(urwid.Frame):
             self.arrow = RIGHT_ARROW
         
     def keypress(self, size, key):
+        # TODO:
+        # obsługa enter przenieść tu z footera - działanie zależne od focusu
         key = super(PyMander, self).keypress(size, key)
-        if key in ('f8', 'q'):
-            raise urwid.ExitMainLoop()
-        elif key == 'left':
+        if key == 'left':
             self.set_active_pane(0)
         elif key == 'right':
             self.set_active_pane(1)
+        elif key == 'f2':
+            # copy
+            self.footer.set_edit("asdf")
+        elif key == 'f3':
+            # move
+            pass
+        elif key == 'f4':
+            # mkdir
+            pass
+        elif key == 'f5':
+            # rename
+            self.set_focus('body')
+        elif key == 'f6':
+            # remove
+            self.set_focus('footer')
+            pass
+        elif key == 'f7':
+            # switch hidden files
+            text = self.footer.original_widget.get_edit_text()
+            self.footer.original_widget.top_w.original_widget.set_caption(text)
+        elif key == 'enter':
+            # TODO: F7 Działa, enter nie działa, bo Edit go obsługuje
+            if self.get_focus == 'footer':
+                text = self.footer.original_widget.get_edit_text()
+                self.footer.original_widget.top_w.original_widget.set_caption(text)
+            else: # 'body'
+                text = self.footer.original_widget.get_edit_text()
+                self.footer.original_widget.top_w.original_widget.set_caption(text)
+        elif key in ('f8', 'q'):
+            raise urwid.ExitMainLoop()
         else:
             return key
