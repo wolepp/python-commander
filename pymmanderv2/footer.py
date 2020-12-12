047e37638724cff917845463bf04fdfe4e57e9a9
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2020-12-13 00:38:10 +0100

    complete overhaul of footer

--- pymmanderv2/footer.py
+++ pymmanderv2/footer.py
@@ -1,34 +1,60 @@
 import urwid
 
+RIGHT_ARROW = ">>"
+LEFT_ARROW = "<<"
+SHOW_HIDDEN_FILES = "show"
+HIDE_HIDDEN_FILES = "hide"
 
-class Footer(urwid.Overlay):
-
+class Footer(urwid.WidgetWrap):
     def __init__(self):
-        bottom_w = urwid.Filler(urwid.Text(self._footer_text(">>", "hide")))
-        top_w = urwid.Filler(urwid.Edit("Caption: "))
-        super(Footer, self).__init__(top_w, bottom_w, 
-            align='left', width=('relative', 100),
-            valign='top', height=2)
+        self.arrow = RIGHT_ARROW
+        self.show_hidden = SHOW_HIDDEN_FILES
+        init_text = Footer._footer_text(self.arrow, self.show_hidden)
+        self.text = urwid.Text(init_text)
+        self.prompt = urwid.Edit("Prompt: ")
+        super(Footer, self).__init__(self.text)
 
-    def update_footer_text(self, arrow, hidden_files):
-        self.top_w.set_text(self._footer_text(arrow))
-
-    def _footer_text(self, arrow, hidden_files):
-        return f"!F2 Copy {arrow} | " + \
-            f"!F3 Move {arrow} | " + \
-             "!F4 Mkdir | " + \
-             "!F5 Rename | " + \
-             "!F6 Remove | " + \
-            f"!F7 {hidden_files} hidden files | " + \
+    @staticmethod
+    def _footer_text(arrow, hidden):
+        return f"F2 Copy {arrow} | " + \
+            f"F3 Move {arrow} | " + \
+             "F4 Mkdir | " + \
+             "F5 Rename | " + \
+             "F6 Remove | " + \
+            f"F7 {hidden} hidden files | " + \
              "F8/q Quit"
 
-    def get_edit_text(self):
-        return self.top_w.original_widget.get_edit_text()
+    def show_prompt(self, prompt_text):
+        self.prompt.set_caption(prompt_text)
+        self.prompt.set_edit_text("")
+        self._w = self.prompt
 
+    def get_edit_text(self):
+        return self.prompt.get_edit_text()
+
+    def show_text(self):
+        self._w = self.text
+
+    def update_text(self, active_pane, show_hidden):
+        if active_pane == 0:
+            self.arrow = RIGHT_ARROW
+        else:
+            self.arrow = LEFT_ARROW
+        if show_hidden:
+            self.show_hidden = HIDE_HIDDEN_FILES    
+        else:
+            self.show_hidden = SHOW_HIDDEN_FILES
+        text = Footer._footer_text(self.arrow, self.show_hidden)
+        self.text.set_text(text)
+        self.show_text()
+            
     def keypress(self, size, key):
-        # obsługa enter przenieść do pymandera - działanie zależne od focusu
         if key == 'enter':
             return key
-        self.top_w.keypress(size, key)
-        if not key in ("q", 'Q'):
+
+        if key == 'esc':
             return key
+
+        key = self._w.keypress(size, key)
+
+        return key
