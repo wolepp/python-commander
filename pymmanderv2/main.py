1e1d2ac3cdbdefb4e2ef396b4e7feb598a13020b
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2020-12-13 00:38:36 +0100

    added help option to run with

--- pymmanderv2/main.py
+++ pymmanderv2/main.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python
 
-import urwid
-from pymander import PyMander
+import argparse
 
 if __name__ == "__main__":
-    
+
+    description = """
+Menadżer plików na wzór Midnight Commandera.
+
+Do działania wymaga pakietu 'urwid'
+Instalacja: `pip install urwid`
+(lub inaczej, w zależności od używanego menadżera pakietów).
+
+Program interaktywny.
+Sterowanie przy pomocy strzałek klawiatury.
+Przechodzenie między folderami przy pomocy Enter.
+
+Program oferuje różne operacje na plikach i folderach (w tym ukrytych):
+kopiowanie, przenoszenie, zmianę nazwy, kasowanie, tworzenie nowych folderów.
+
+Operacja kasowania wymaga potwierdzenia (kasuje nieodwracalnie, tak jak 'rm').
+Można anulować wykonywanie operacji kasowania, tworzenia nowego folderu lub zmiany nazwy przez wciśnięcie Escape.
+    """
+
+    parser = argparse.ArgumentParser(
+        description=description,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    args = parser.parse_args()
+
+    import urwid
+    from pymander import PyMander
     pm = PyMander()
 
     palette = [
